257d006088a6c86845b89169560da1ebdd176778
yashrajbharticybtekk <y.bharti@cybtekk.com>
2023-08-11 03:47:10 +0530

    leaflet map code changes

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-if (navigator.geolocation) {
-  let map = L.map("map").setView([41.661254, -0.892912], 13);
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
-}
+let map = L.map("map").setView([41.661254, -0.892912], 13);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
 let coordsToCopy;
 
 function getUserLocation() {
